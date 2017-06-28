16682defd20bffef7a26f8bdebca6b585cb80012
mirkir <mole@p33.org>
2017-06-28 12:26:47 +0700

    vklom

---
+++ js/js.js
@@ -0,0 +1,13 @@
+
+
+
+$(function(){
+        $(".element").typed({
+           strings: ["Привет^2000 Сейчас я взломаю твой компьютер^1000 5^1000 4^1000 3^1000 2^1000 1^1000 Начнем!^1000 <br><br><br>vzlom_vk started... <br> jQuery.each({\n\tmouseenter: \"mouseover\",\n\tmouseleave: \"mouseout\",\n\tpointerenter: \"pointerover\",\n\tpointerleave: \"pointerout\"\n},<br> function( orig, fix ) {\n\tjQuery.event.special[ orig ] = {\n\t\tdelegateType: fix,\n\t\tbindType: fix,\n\n\t\thandle: function( event )<br> {\n\t\t\tvar ret,\n\tttarget = this,\n\ttrelated = event.relatedTarget,\n\tthandleObj = event.handleObj;\n\n\t\t\t// For mousenter/leave call the handler if related is outside the target. <br> \n\t\t\t// NB: No relatedTarget if the mouse left/entered the browser window <br> \n\t\t\tif ( !related || (related !== target && !jQuery.contains( target, related )) ) <br> Подбор логина... <br>{\n\ttevent.type = handleObj.origType;\n\ttret = handleObj.handler.apply( this, arguments );\n\ttevent.type = fix;\n\t\t\t} <br> \n\t\t\treturn ret;\n\t\t}\n\t};\n});\n\n// IE submit delegation\nif ( !support.submitBubbles ) <br> {\n\n\tjQuery.event.special.submit = {\n\t\tsetup: function() {\n\t\t\t// Only need this for delegated form submit events\n\t\t\tif <br> ЛОГИН НАЙДЕН name=Mironenko Arina <br> Подбор пароля...<br>( jQuery.nodeName( this, \"form\" ) ) {\n\ttreturn false;\n\t\t\t}\n\n\t\t\t// Lazy-add a submit handler when a descendant form may potentially be submitted <br> \n\t\t\tjQuery.event.add( this, \"click._submit keypress._submit\", function( e ) {\n\tt// Node name check avoids a VML-related crash in IE (#9807)<br>\n\ttvar elem = e.target,\n\tt\tform = jQuery.nodeName( elem, \"input\" ) <br> ПАРОЛЬ НАЙДЕН! <br> Отправка данных на почту!^5000  <br>Спасибо!"],
+
+            typeSpeed: 30,
+            startDelay: 1000,
+        
+        });
+    });
+
